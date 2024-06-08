b56c7d7884e7d5a43cc48b05a114887ba6a464ec
Victor S <victor.skarerverket@gmail.com>
2024-06-09 00:37:26 +0200

    Added a ViewModel for Machines

---
+++ Industriell_Maskinpark_API/ViewModels/MachineViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Industriell_Maskinpark_API.Models;
+using System.Reflection.PortableExecutable;
+
+namespace Industriell_Maskinpark_API.ViewModels
+{
+    public class MachineViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime LatestContact { get; set; }
+        public string LastMessage { get; set; }
+
+        public MachineViewModel(Models.Machine machine)
+        {
+            this.Id = machine.Id;
+            this.Name = machine.Name;
+            if (machine.Status) { Status = "Online"; }
+            else { Status = "Offline"; }
+            this.LatestContact = machine.LatestContact;
+            this.LastMessage = machine.LastMessage;
+        }
+
+        public MachineViewModel(Guid id, string name, bool status, DateTime latestContact, string lastMessage)
+        {
+            this.Id = id;
+            this.Name = name;
+            if(status) { Status = "Online"; }
+            else { Status = "Offline"; }
+            this.LatestContact = latestContact;
+            this.LastMessage = lastMessage;
+        }
+    }
+
+}
