0a934eba952aea2bb07ecfcfa5759c01d2d6f1f5
Victor S <victor.skarerverket@gmail.com>
2024-06-10 01:22:50 +0200

    Saving work. Buttons WIP.
    Also fixed icon & company name alignment

--- Industriell_Maskinpark_Blazor/Industriell_Maskinpark_Blazor/Services/MachineService.cs
+++ Industriell_Maskinpark_Blazor/Industriell_Maskinpark_Blazor/Services/MachineService.cs
@@ -15,5 +15,19 @@ namespace Industriell_Maskinpark_Blazor.Services
         {
             return await _httpClient.GetFromJsonAsync<List<Machine>>("api/Machines");
         }
+
+        public async Task<bool> DeleteMachine(Guid guid)
+        {
+            var result = await _httpClient.DeleteAsync("api/Machines/" + guid.ToString());
+
+            if (result.StatusCode == System.Net.HttpStatusCode.NoContent)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
